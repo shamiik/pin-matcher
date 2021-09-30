f8188dcbe091504dd643791efa147bd9cb65bbef
shamiik <shamikdey7@gmail.com>
2021-09-30 10:56:32 +0600

    event bubbled & matching condition

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 function getPin() {
     const pin = Math.round(Math.random() * 10000);
+    // console.log(pin)
     const pinString = pin + '';
     if (pinString.length == 4) {
         return pin;
@@ -13,5 +14,39 @@ function generatePin() {
     const pin = getPin();
     // console.log(pin)
     document.getElementById('display-pin').value = pin;
+}
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    // console.log('clicked')
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-numbers');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calcInput.value = '';
+        }
+        // console.log(number);
+    }
+    else {
 
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+
+})
+
+function verifyPin() {
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+    const successNotification = document.getElementById('notify-success');
+    const failNotification = document.getElementById('notify-fail');
+    if (pin == typedNumbers) {
+        // console.log('matched')
+        successNotification.style.display = 'block';
+        failNotification.style.display = 'none';
+    }
+    else {
+        // console.log('ops');
+        failNotification.style.display = 'block';
+        successNotification.style.display = 'none';
+    }
 }
